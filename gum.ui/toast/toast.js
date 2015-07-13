ffa3b1288d3aef62048f7f57149499fdb8de4b21
潘盛 <pansheng@live.cn>
2015-07-13 11:12:07 +0800

    update

--- gum.ui/toast/toast.js
+++ gum.ui/toast/toast.js
@@ -1,8 +1,4 @@
 "use strict";
-/**
- *
- **/
-//var tpl = require('toast.tpl');
 
 /**
  * 自动消失提示框
@@ -16,7 +12,7 @@ Toast.prototype = {
     /**
      * 自动消失提示框
      * @function
-     * @name Show
+     * @name showToast
      * @param {string} message - 提示框文字内容
      * @param {int} timeout - 关闭时间毫秒,选填默认2000
      */
@@ -44,6 +40,43 @@ Toast.prototype = {
         setTimeout(function () {
             document.body.removeChild(toast);
         },timeOut);
+    },
+    /**
+     * 带菊花的自动消失提示框
+     * @function
+     * @name showLoadingToast
+     * @param {string} message - 提示框文字内容
+     * @param {int} timeout - 关闭时间毫秒,选填默认2000
+     */
+    showLoadingToast :function(/* message, timeout */){
+        // 定义提示信息
+        var message = arguments[0];
+
+        // 定义一个提示框div
+        var toast = document.createElement('div');
+
+        // 定义css
+        toast.classList.add('toast-container');
+
+        // 添加文字信息
+        toast.innerHTML = "<div class='toast-message'><span class='spinner-white'></span>" + message + "</div>";
+
+        // 添加到窗体内
+        document.body.appendChild(toast);
+
+        if (arguments.length === 2)
+        {
+            // 定义延迟时间
+            var timeOut = arguments[1];
+
+            // 自动关闭
+            setTimeout(function () {
+                document.body.removeChild(toast);
+            },timeOut);
+        }
+    },
+    hideLoadingToast :function(toast) {
+        document.body.removeChild(toast);
     }
 };
 
